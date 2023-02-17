fb64f0e9e52898482b0749e34f1e271edf4f4181
AdvikCode22 <94784477+AdvikCode22@users.noreply.github.com>
2023-02-17 17:06:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+lwristx = ""
+rwristx = ""
+lwristy = ""
+rwristy = ""
+
 function setup(){
     canvas = createCanvas(600,500)
     canvas.center()
 
     camera = createCapture(VIDEO)
     camera.hide()
+
+    model = ml5.poseNet(camera , modelLoaded)
+    model.on('pose' , gotPoses)
 }
 
 function draw(){
@@ -13,4 +21,22 @@ function draw(){
 function preload(){
     music1 = loadSound("potc.mp3")
     music2 = loadSound("hp.mp3")
+}
+
+function modelLoaded(){
+    console.log("Model Has been loaded.")
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results)
+
+        lwristx = results[0].pose.leftWrist.x
+        lwristy = results[0].pose.leftWrist.y
+        rwristx = results[0].pose.rightWrist.x
+        rwristy = results[0].pose.rightWrist.y
+
+        console.log("left wrist x = " + lwristx + " , left wrist y = " + lwristy)
+        console.log("right wrist x = " + rwristx + " , right wrist y = " + rwristy)
+    }
 }
